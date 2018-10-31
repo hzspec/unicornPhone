216ad85c5afde3102aea87aa499f28defe7c6dd1
Your Name <you@example.com>
2018-10-31 18:35:56 +0800

    add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,26 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { Storage } from '@ionic/storage';
 declare var screen :any;
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = 'LoginPage';
+  rootPage:any = '';
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, 
+    private storage: Storage) {
+
+    this.storage.get('isShow').then((val:any)=>{
+      if(val === true){
+        this.rootPage = 'LoginPage';
+      }else{
+        this.rootPage = 'SliderinfoPage';
+      }
+    });
+
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
